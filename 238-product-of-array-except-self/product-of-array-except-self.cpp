035f6fb74c6b111829ecd43962e2a0c76c5eca40
INZEMAM KHAN <42077809+inzemamk1997@users.noreply.github.com>
2024-07-06 18:19:18 +0530

    Time: 28 ms (53.40%) | Memory: 40.2 MB (12.08%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -2,27 +2,20 @@ class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
         int n = nums.size();
-        vector<int>  ans(n);
-        int currPrefix = 1, currSuffix = 1;
-        for(int i=0; i<n; i++){
-            if(i==0)
-                currPrefix = 1;
-            else
-                currPrefix = nums[i-1]*currPrefix;
-
-            ans[i] = currPrefix;
+        vector<int> prefixProduct(n,1), suffixProduct(n,1), ans(n, 1);
+        int maxProduct = 1;
+        for(int i = 1; i < n; i++){
+            maxProduct *= nums[i-1];
+            prefixProduct[i] = maxProduct;
         }
-
-        for(int i=n-1; i>=0; i--){
-            if(i==n-1)
-                currSuffix = 1;
-            else
-                currSuffix = nums[i+1]*currSuffix;
-
-            ans[i] = ans[i] * currSuffix;
+        maxProduct = 1;
+        for(int i = n-2; i >= 0; i--){
+            maxProduct *= nums[i+1];
+            suffixProduct[i] = maxProduct;
         }
-        
-        
+        for(int i = 0; i < n; i++){
+            ans[i] = suffixProduct[i] * prefixProduct[i];
+        }
         return ans;
     }
 };
