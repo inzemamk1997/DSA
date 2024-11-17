cb9d4b9684b9afda730f0301b1e551003544f553
INZEMAM KHAN <42077809+inzemamk1997@users.noreply.github.com>
2024-11-17 16:42:45 +0530

    Time: 5 ms (11.81%) | Memory: 15.4 MB (10.19%) - LeetSync

--- 871-keys-and-rooms/keys-and-rooms.cpp
+++ 871-keys-and-rooms/keys-and-rooms.cpp
@@ -1,22 +1,34 @@
 class Solution {
 public:
-    // Depth First Search function to visit rooms
-    void dfs(int i, vector<vector<int>>& rooms, vector<bool> &cont) {
-        cont[i] = true; // Mark room i as visited
-        for(auto j : rooms[i]) { // Explore all keys in room i
-            if(!cont[j]) dfs(j, rooms, cont); // Visit the room corresponding to the key if not already visited
-        }
-    }
-    
-    // Function to check if all rooms can be visited
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         int n = rooms.size();
-        vector<bool> cont(n, false); // Create a visited array to track visited rooms
-        cont[0] = true; // Mark room 0 as initially visited
-        dfs(0, rooms, cont); // Start DFS from room 0
-        for(auto j : cont) { // Check if all rooms have been visited
-            if(!j) return false; // If any room is not visited, return false
+        vector<vector<int> > adj(n);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<rooms[i].size(); j++){
+                adj[i].push_back(rooms[i][j]);
+            }
         }
-        return true; // If all rooms are visited, return true
+
+        vector<bool> visited(n, false);
+
+        queue<int> q;
+        q.push(0);
+
+        while(!q.empty()){
+            int room = q.front();
+            q.pop();
+            if(!visited[room]){
+                visited[room] = true;
+                for(int j : rooms[room])
+                    if(!visited[j])
+                        q.push(j);
+            }
+        }
+
+        for(bool v : visited)
+            if(!v)
+                return false;
+        
+        return true;
     }
 };
