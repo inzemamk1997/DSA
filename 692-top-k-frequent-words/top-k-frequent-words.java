7b34f1463d7df3fa77613cb7c311f7ce67ce09e9
INZEMAM KHAN <42077809+inzemamk1997@users.noreply.github.com>
2025-05-21 23:28:24 +0530

    Time: 6 ms (98.30%) | Memory: 44.8 MB (55.23%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,38 @@
+class Solution {
+    class Pair {
+        int first;
+        String second;
+
+        Pair(int first, String second){
+            this.first = first;
+            this.second = second;
+        }
+    }
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> mp = new HashMap<>();
+        int n = words.length;
+        for(int i = 0; i < n; i++){
+            mp.put(words[i], mp.getOrDefault(words[i], 0) + 1);
+        }
+       PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> {
+            if (a.first != b.first) {
+                return a.first - b.first; // min-heap by frequency
+            } else {
+                return b.second.compareTo(a.second); // reverse lex order
+            }
+        });
+        for(Map.Entry<String, Integer> item : mp.entrySet()){
+            pq.add(new Pair(item.getValue(), item.getKey()));
+            if(pq.size() > k)
+                pq.poll();
+        }
+        
+        
+        List<String> ans = new ArrayList<>();
+        while(!pq.isEmpty()){
+            ans.add(pq.poll().second);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
