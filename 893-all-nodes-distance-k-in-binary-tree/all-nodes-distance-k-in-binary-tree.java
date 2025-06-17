43327a307e8a146f6a8fdd221929c9138bd613fb
INZEMAM KHAN <42077809+inzemamk1997@users.noreply.github.com>
2025-06-17 23:33:54 +0530

    Time: 13 ms (66.09%) | Memory: 42.1 MB (96.36%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -8,49 +8,48 @@
  * }
  */
 class Solution {
-    HashMap<TreeNode, TreeNode> parent = new HashMap<>();
-    void inorder(TreeNode root){
+    void inorder(TreeNode root, Map<TreeNode, TreeNode> mp){
         if(root == null)
             return;
-        if(root.left != null){
-            parent.put(root.left, root);
-        }
-        inorder(root.left);
-        if(root.right != null){
-            parent.put(root.right, root);
-        }
-        inorder(root.right);
+        if(root.left != null)
+            mp.put(root.left, root);
+        if(root.right != null)
+            mp.put(root.right, root);
+        inorder(root.left, mp);
+        inorder(root.right, mp);
     }
-    
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
         List<Integer> ans = new ArrayList<>();
-        inorder(root);
-        ArrayDeque<TreeNode> q = new ArrayDeque<>();
-        q.add(target);
+        ArrayDeque<TreeNode> dq = new ArrayDeque<>();
+        Map<TreeNode, TreeNode> mp = new HashMap<>();
         Set<TreeNode> visited = new HashSet<>();
+        inorder(root, mp);
+        if(target == null)
+            return ans;
+        dq.add(target);
         visited.add(target);
-        while(!q.isEmpty() && k != 0){
-            int n = q.size();            
-            for(int i = 0; i < n; i++){  
-                TreeNode curr = q.poll();
-                if(curr.left != null && !visited.contains(curr.left)){                 
-                    q.add(curr.left);
-                    visited.add(curr.left);
+        for(int i = 0; i < k; i++){
+            int sz = dq.size();
+            for(int j = 0; j < sz; j++){
+                TreeNode t = dq.pop();
+                if(t.left != null && !visited.contains(t.left)){
+                    dq.add(t.left);
+                    visited.add(t.left);
                 }
-                if(curr.right != null && !visited.contains(curr.right)){                 
-                    q.add(curr.right);
-                    visited.add(curr.right);
+                if(t.right != null && !visited.contains(t.right)){
+                    dq.add(t.right);
+                    visited.add(t.right);
                 }
-                TreeNode p = parent.get(curr);
-                if(p != null && !visited.contains(p)){
-                    q.add(p);
-                    visited.add(p);
+                if(mp.containsKey(t) && !visited.contains(mp.get(t)) ){
+                    dq.add(mp.get(t));
+                    visited.add(mp.get(t));
                 }
             }
-            k--;
         }
-        while(!q.isEmpty())
-            ans.add(q.poll().val);
+        while(!dq.isEmpty()){
+            TreeNode t = dq.pop();
+            ans.add(t.val);
+        }
         return ans;
     }
 }
