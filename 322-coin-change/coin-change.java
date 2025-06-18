36e90dd40814308ddc512df2415e542b1effb167
INZEMAM KHAN <42077809+inzemamk1997@users.noreply.github.com>
2025-06-18 17:50:17 +0530

    Time: 26 ms (31.01%) | Memory: 45.4 MB (21.68%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -18,7 +18,7 @@ class Solution {
         Arrays.fill(dp, -1);
         long ans = minCoin(coins, amount);
         //System.out.println(amount);
-        if(dp[amount] >= Integer.MAX_VALUE)
+        if(dp[amount] == Integer.MAX_VALUE)
             return -1;
         return (int)dp[amount];
     }
