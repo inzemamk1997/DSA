f523f847c301a0b930b69a55fd691475fb26b5ca
INZEMAM KHAN <42077809+inzemamk1997@users.noreply.github.com>
2026-01-18 14:08:27 +0530

    Time: 8 ms (38.20%) | Memory: 58.6 MB (5.11%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -2,24 +2,21 @@ class Solution {
     public boolean isAlphaNum(char c){
         return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9'));
     }
+    public boolean check(String s, int i, int j){
+        if(i >= j )
+            return true;
+        
+        if(!isAlphaNum(s.charAt(i)))
+            return check(s, i + 1, j);
+        
+        if(!isAlphaNum(s.charAt(j)))
+            return check(s, i, j-1);
+        
+        return ((Character.toLowerCase(s.charAt(i)) == Character.toLowerCase(s.charAt(j))) && check(s, i + 1, j - 1));
+    }
     public boolean isPalindrome(String s) {
         int l = s.length();
         int i = 0, j = l - 1;
-
-        while (i <= j){
-            char start = s.charAt(i);
-            char end = s.charAt(j);
-
-            if(!isAlphaNum(start))
-                i++;
-            else if(!isAlphaNum(end))
-                j--;
-            else{
-                if(Character.toLowerCase(start) != Character.toLowerCase(end))
-                    return false;
-                i++;j--;
-            }
-        }
-        return true;
+        return check(s, i, j);
     }
 }
