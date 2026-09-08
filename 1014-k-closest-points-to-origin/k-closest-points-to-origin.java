0fac37771a9f37df458b6b95e838b9828d1e05ce
INZEMAM KHAN <42077809+inzemamk1997@users.noreply.github.com>
2026-09-08 20:08:49 +0530

    Time: 27 ms (75.04%) | Memory: 54.3 MB (51.87%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,24 +1,27 @@
 class Solution {
-    public int[][] kClosest(int[][] points, int k) {
-        int[][] ans = new int[k][2];
-        int n = points.length;
-        PriorityQueue<long[]> pq = new PriorityQueue<>((a,b) -> Long.compare(b[0],a[0]));
-        for(int i = 0; i < n; i++){
-            int x = points[i][0];
-            int y = points[i][1];
-            
-            long d = x*x + y*y;
-            pq.add(new long[]{d, x, y});
-            int sz = pq.size();
-            if(sz > k){
-                pq.poll();        
-            }
+    public class Distance{
+        public Double d;
+        public int[] p;
+
+        public Distance(Double d, int[] p){
+            this.d = d;
+            this.p = p;
         }
-        for(int i = 0; i < k; i++){
-            long[] x = new long[3];
-            x = pq.poll();
-            ans[i][0] = (int)x[1];
-            ans[i][1] = (int)x[2];
+    }
+    public Double computeDistance(int[] point){
+        return Math.sqrt(point[0] * point[0] + point[1] * point[1]);
+    }
+    public int[][] kClosest(int[][] points, int k) {
+        PriorityQueue<Distance> pq = new PriorityQueue<Distance>((a,b) -> Double.compare(a.d,b.d)); 
+        for (int[] point : points){
+            Double dist = computeDistance(point);
+            Distance d = new Distance(dist, point);
+            pq.add(d);
+        }
+        int[][] ans = new int[k][2];
+        for(int i =0; i<k; i++){
+            Distance d = pq.poll();
+            ans[i] = d.p;
         }
         return ans;
     }
